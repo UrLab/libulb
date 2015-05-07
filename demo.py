42f7d17913c3921af8d6e1102add39d739e530d2
Nikita Marchant <nikita.marchant@gmail.com>
2015-05-07 14:52:19 +0200

    actualy add examples

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import libulb
+from datetime import datetime
 
 c = libulb.Client.auth('adelcha', 'pAssW0r!')
 
@@ -7,3 +8,8 @@ print(info['name']) # Alain
 
 inscriptions = c.inscriptions()
 notes_first_year = c.notes(inscriptions[0])
+
+c.note_detail(c.notes(c.inscriptions()[-1])[-1]) # Details of the last course of your last year
+
+c.gehol(datetime(2015, 05, 05, 0, 0, 0), datetime(2015, 05, 06, 0, 0, 0)) # All of your courses of the day
+
