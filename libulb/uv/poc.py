99bea50c9a0bf381ee5623f10c49d28bd865e560
Nikita Marchant <nikita.marchant@gmail.com>
2016-05-30 16:01:33 +0200

    Add structure

--- libulb/uv/poc.py
+++ libulb/uv/poc.py
@@ -1,25 +1,39 @@
 import requests
-from bs4 import
+from bs4 import BeautifulSoup
 import re
+import urllib
 
-s = requests.Session()
-s.post("https://uv.ulb.ac.be/login/index.php", data=payload)
+def get_session(netid, password):
+    payload={
+        "username": netid,
+        "password": password,
+    }
 
+    s = requests.Session()
+    s.post("https://uv.ulb.ac.be/login/index.php", data=payload)
 
-# List categories
-categories_html = s.get("http://uv.ulb.ac.be/course/")
-soup = BeautifulSoup(categories_html.text, "html.parser")
-dropdowns = soup.findAll(class_="info")
+    return s
 
-categories = [(cat.find('a')['href'], cat.find('a').text) for cat in dropdowns]
+def _chop_category(category):
+    url = category.find('a')['href']
+    split = urllib.parse.urlsplit(url)
+    qs = urllib.parse.parse_qs(split.query)
 
+    return {
+        "url": url,
+        "name": category.find('a').text,
+        "id": int(qs["categoryid"][0])
+    }
 
-# List courses of categories
-section = s.get("http://uv.ulb.ac.be/course/index.php?categoryid=68&perpage=5000&browse=courses")
-soup = BeautifulSoup(section.text, "html.parser")
+def _list_categories(s):
+    categories_html = s.get("http://uv.ulb.ac.be/course/")
+    soup = BeautifulSoup(categories_html.text, "html.parser")
+    dropdowns = soup.findAll(class_="info")
 
+    categories = [_chop_category(cat) for cat in dropdowns]
+    return categories
 
-def chop_course(course):
+def _chop_course(course):
     course_link = course.find(class_="coursename").find("a")
     mnemo = None
     m = re.match(r'(\w{4})-(\w)(\d{3,4})', course_link.text)
@@ -32,43 +46,59 @@ def chop_course(course):
     else:
         name = course_link.text
 
+    url = course_link['href']
+    split = urllib.parse.urlsplit(url)
+    qs = urllib.parse.parse_qs(split.query)
+
     name = re.match(r'^(\w{4}-\w\d{3,4})?( - )?(.*)$', name).groups()[-1].strip().replace("  ", "").replace("  ", " ")
     return {
         "mnemo": mnemo,
         "name": name,
-        "url": course_link['href'],
+        "url": url,
+        "id": int(qs["id"][0]),
         "can_register": course.find(title="Auto-inscription") is not None,
     }
 
+def _list_category_courses(s, category_id):
+    # List courses of categories
+    section = s.get("http://uv.ulb.ac.be/course/index.php?categoryid=%i&perpage=5000&browse=courses" % category_id)
+    soup = BeautifulSoup(section.text, "html.parser")
 
-courses = [chop_course(c) for c in soup.findAll(class_="coursebox")]
-
-
-# Enrol
-course_page = s.get("http://uv.ulb.ac.be/course/view.php?id=51823")
-if "enrol" in course_page.url:
-    enrol_url = "http://uv.ulb.ac.be/enrol/index.php"
-    soup = BeautifulSoup(course_page.text, "html.parser")
-    form = soup.find("form", action=enrol_url)
-    fields = form.findAll('input')
-    payload = {x['name']: x['value'] for x in fields}
-    s.post(enrol_url, data=payload)
-else:
-    print("Alredy subscribed")
+    courses = [_chop_course(c) for c in soup.findAll(class_="coursebox")]
+    return courses
 
 
-#Unenrol
-course_page = s.get("http://uv.ulb.ac.be/course/view.php?id=51823")
-if "enrol" not in course_page.url:
-    soup = BeautifulSoup(course_page.text, "html.parser")
-    link = soup.find("a", href="http://uv.ulb.ac.be/enrol/self/unenrolself.php?enrolid=237082")
+def subscribe(s, course_id):
+    course_page = s.get("http://uv.ulb.ac.be/course/view.php?id=%i" % course_id)
+    if "enrol" in course_page.url:
+        enrol_url = "http://uv.ulb.ac.be/enrol/index.php"
+        soup = BeautifulSoup(course_page.text, "html.parser")
+        form = soup.find("form", action=enrol_url)
+        fields = form.findAll('input')
+        payload = {x['name']: x['value'] for x in fields}
+        s.post(enrol_url, data=payload)
+    else:
+        raise RuntimeError("User was already subscribed")
 
-    unrol_page = s.get(link['href'])
-    unrol_url = "http://uv.ulb.ac.be/enrol/self/unenrolself.php"
-    soup = BeautifulSoup(unrol_page.text, "html.parser")
-    form = soup.find("form", action=unrol_url)
-    fields = form.findAll('input')
-    payload = {x.get('name',""): x['value'] for x in fields}
-    s.post(unrol_url, data=payload)
-else:
-    print("Alredy unsubscribed")
+
+def unsubscribe(s, course_id):
+    course_page = s.get("http://uv.ulb.ac.be/course/view.php?id=%i" % course_id)
+    if "enrol" not in course_page.url:
+        soup = BeautifulSoup(course_page.text, "html.parser")
+        has_unrol = lambda x: "http://uv.ulb.ac.be/enrol/self/unenrolself.php" in x['href']
+        link = list(filter(has_unrol, soup.findAll("a")))[0]
+
+        unrol_page = s.get(link['href'])
+        unrol_url = "http://uv.ulb.ac.be/enrol/self/unenrolself.php"
+        soup = BeautifulSoup(unrol_page.text, "html.parser")
+        form = soup.find("form", action=unrol_url)
+        fields = form.findAll('input')
+        payload = {x.get('name',""): x['value'] for x in fields}
+        s.post(unrol_url, data=payload)
+    else:
+        raise RuntimeError("User was already unsubscribed")
+
+
+def is_subscribed(s, course_id):
+    course_page = s.get("http://uv.ulb.ac.be/course/view.php?id=%i" % course_id)
+    return "enrol" not in course_page.url
