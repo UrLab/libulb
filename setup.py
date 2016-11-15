041409e9d3e1d14c30ac66069673c49d1969709c
Nikita Marchant <nikita.marchant@gmail.com>
2016-11-15 17:34:24 +0100

    0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", 'r') as fd:
 
 setup(
     name='libulb',
-    version='0.1.3.dev',
+    version='0.1.3',
     description='Interact with ULB in python',
     url='https://github.com/C4ptainCrunch/libulb',
     author='Nikita Marchant',
